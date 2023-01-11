da194cd9fdd5e574e27094bfd404ffdde0970a51
Shlee <shlee0125@postech.ac.kr>
2023-01-11 15:26:58 +0900

    tutorial

--- tutorial_notebooks/utils/generate_data.py
+++ tutorial_notebooks/utils/generate_data.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 class PointCloud():
-    def __init__(self, n_samples = 1000, n_points = 'random', manifolds=['sphere'],  noise = 5.0):
+    def __init__(self, n_samples = 1000, n_points = 'random', manifolds=['sphere'],  noise = 0.05):
         self.n_samples = n_samples
         self.n_points = n_points
         self.noise = noise
@@ -52,6 +52,73 @@ class PointCloud():
             sample[:, 0] = (2*np.cos(s)+4)*np.cos(t)   # x
             sample[:, 1] = (2*np.cos(s)+4)*np.sin(t)   # y
             sample[:, 2] = 2*np.sin(s)             # z
+
+            # random noise
+            noise = np.random.normal(0, self.noise, size=sample.shape)
+            data.append(sample + noise)
+        return data
+
+    def mobius(self):
+        '''
+        Generate point cloud examples.
+        '''
+        data = []
+
+        for _ in range(self.n_samples):
+            if self.n_points == 'random':
+                n_points = np.random.randint(1000,2000,1)[0]
+            
+            sample = np.zeros([n_points, 3])
+            
+            # domain of parameters
+            t = np.random.uniform(-0.5, 0.5, n_points)
+            s = np.random.uniform(0.0, 2*np.pi, n_points)
+        
+            # parametric equation of sphere
+            sample[:, 0] = (1-t*np.sin(s/2))*np.cos(s)   # x
+            sample[:, 1] = (1-t*np.sin(s/2))*np.sin(s)   # y
+            sample[:, 2] = t*np.cos(s/2)             # z
+
+            # random noise
+            noise = np.random.normal(0, self.noise, size=sample.shape)
+            data.append(sample + noise)
+        return data
+
+    def klein_bottle(self):
+        '''
+        Generate point cloud examples.
+        '''
+        data = []
+
+        for _ in range(self.n_samples):
+            if self.n_points == 'random':
+                n_points = np.random.randint(1000,2000,1)[0]
+            
+            sample = np.zeros([n_points, 3])
+            
+            # domain of parameters
+            u = np.random.uniform(0.0, np.pi, n_points)
+            v = np.random.uniform(0.0, 2*np.pi, n_points)
+        
+            # parametric equation of sphere
+            sample[:, 0] = (-(2/15) * np.cos(u) * 
+                                (3 * np.cos(v) 
+                                - 30* np.sin(u) 
+                                + 90 * (np.cos(u) ** 4) * np.sin(u)
+                                - 60 * (np.cos(u) ** 6) * np.sin(u)
+                                + 5 * np.cos(u) * np.cos(v) * np.sin(u)))  # x
+            sample[:, 1] = (-(1/15) * np.sin(u) * 
+                                (3 * np.cos(v) 
+                                - 3* (np.cos(u) **2) * np.cos(v) 
+                                - 48 * (np.cos(u) ** 4) * np.cos(v)
+                                + 48 * (np.cos(u) ** 6) * np.cos(v)
+                                - 60 * np.sin(u)
+                                + 5 * np.cos(u) * np.cos(v) * np.sin(u)
+                                - 5 * (np.cos(u) ** 3) * np.cos(v) * np.sin(u)
+                                - 80 * (np.cos(u) ** 5) * np.cos(v) * np.sin(u)
+                                + 80 * (np.cos(u) ** 7) * np.cos(v) * np.sin(u)
+                                ))   # y
+            sample[:, 2] = (2/15) * (3 + 5 * np.cos(u) * np.sin(u)) * np.sin(v)         # z
 
             # random noise
             noise = np.random.normal(0, self.noise, size=sample.shape)
